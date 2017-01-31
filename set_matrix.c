d4b06d97ca26ced95a1623a320ec4707bae08ff5
Louis Young <youngalou@gmail.com>
2017-01-30 16:42:13 -0800

    2D matrix has been written!

--- set_matrix.c
+++ set_matrix.c
@@ -6,7 +6,7 @@
 /*   By: lyoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 15:31:07 by lyoung            #+#    #+#             */
-/*   Updated: 2017/01/30 16:05:43 by lyoung           ###   ########.fr       */
+/*   Updated: 2017/01/30 16:37:56 by lyoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,19 +101,74 @@ char	*set_array(char *file, int size)
 	return (array);
 }
 
+char	**set_matrix(int height, int width)
+{
+	int		i;
+	char	**matrix;
+
+	i = 0;
+	matrix = (char**)malloc(height * sizeof(matrix));
+	while (i < height)
+	{
+		matrix[i] = (char*)malloc(width * sizeof(matrix));
+		i++;
+	}
+	return (matrix);
+}
+
+char	**fill_matrix(char **matrix, char *array)
+{
+	int		i;
+	int		y;
+	int		x;
+
+	i = 0;
+	y = 0;
+	x = 0;
+	while (array[i] != '\n')
+		i++;
+	i++;
+	while (array[i])
+	{
+		if (array[i] != '\n')
+		{
+			matrix[y][x] = array[i];
+			x++;
+		}
+		else
+		{
+			y++;
+			x = 0;
+		}
+		i++;
+	}
+	return (matrix);
+}
+
 int		main(int argc, char **argv)
 {
+	int		i;
 	int		size;
 	int		height;
 	int		width;
 	char	*array;
+	char	**matrix;
 
 	(void)argc;
+	i = 0;
 	size = find_size(argv[1]);
 	array = set_array(argv[1], size);
 	height = find_height(argv[1]);
 	width = find_width(argv[1], size, height);
+	matrix = set_matrix(height, width);
+	matrix = fill_matrix(matrix, array);
 	printf("height = %d\nwidth = %d\n", height, width);
+	while (i < height)
+	{
+		ft_putstr(matrix[i]);
+		ft_putchar('\n');
+		i++;
+	}
 	ft_putstr(array);
 	return (0);
 }
